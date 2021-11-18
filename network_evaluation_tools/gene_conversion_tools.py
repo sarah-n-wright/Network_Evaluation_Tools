237f67483604564498d3960be0a17b6eca8fc59e
sarah-n-wright <sarahwright474@gmail.com>
2021-11-18 13:55:42 -0800

    Updated for python 3, latest pandas, current mygene.info API, tested with original GeneMANIA dataset

--- network_evaluation_tools/gene_conversion_tools.py
+++ network_evaluation_tools/gene_conversion_tools.py
@@ -5,6 +5,9 @@ import requests
 import re
 import time
 import pandas as pd
+import numpy as np
+from tqdm import tqdm
+import json
 
 # Determine if id to be input is a valid gene name (does not contain parentheses or quotations or whitespace)
 def exclude_id(name, bad_prefixes=None):
@@ -20,7 +23,7 @@ def exclude_id(name, bad_prefixes=None):
 def get_identifier_without_prefix(string):
 	elements = string.split(':')
 	length = len(elements)
-	if length is 2:
+	if length == 2:
 		return str(elements[1])
 	elif length > 2:
 		return None
@@ -42,42 +45,41 @@ def query_constructor(gene_list, exclude_prefixes=None, print_invalid_genes=Fals
 	query_string = ' '.join(valid_query_genes) # Build string of names to input into MyGene.Info
 	return query_string, valid_query_genes, invalid_query_genes
 
-# Function for posting batch query to MyGene.info v3.0.0 API
-def query_batch(query_string, tax_id='9606', scopes="symbol, entrezgene, alias, uniprot", fields="symbol, entrezgene"):
-	query_split = query_string.split(' ')
-	query_n = len(query_split)
-	query_time = time.time()
-	if query_n <=1000:
-		data = {'species': tax_id, # Human Only
-				'scopes': scopes, # Default symbol, entrez, alias, uniprot. Alias often returns more genes than needed, return only higest scoring genes
-				'fields': fields, # Which gene name spaces to convert to
-				'q': query_string}
-		res = requests.post('http://mygene.info/v3/query', data)
-		json = res.json()
-	else:
-		# If the query is too long, we will need to break it up into chunks of 1000 query genes (MyGene.info cap)
-		if query_n % 1000 == 0:
-		    chunks = query_n / 1000
-		else:
-		    chunks = (query_n / 1000) + 1
-		query_chunks = []
-		for i in range(chunks):
-		    start_i, end_i = i*1000, (i+1)*1000
-		    query_chunks.append(' '.join(query_split[start_i:end_i]))
-		json = []
-		for chunk in query_chunks:
-		    data = {'species': '9606', # Human Only
-		        	'scopes': "entrezgene, retired", # Default symbol, entrez, alias, uniprot. Alias often returns more genes than needed, return only higest scoring genes
-		        	'fields': "symbol, entrezgene", # Which gene name spaces to convert to
-		        	'q': chunk}
-		    res = requests.post('http://mygene.info/v3/query', data)
-		    json = json+res.json()		    
-	print(len(json), 'Matched query results')
-	print('Batch query complete:', round(time.time()-query_time,2), 'seconds')
-	return json
+# Function for posting batch query to MyGene.info v3 API (2021)
+def query_batch(query_string, tax_id='9606', scopes="symbol,entrezgene,alias,uniprot", fields="symbol,entrezgene"):
+    query_split = query_string.split(' ')
+    query_n = len(query_split)
+    query_time = time.time()
+    if query_n <=1000:
+        q = ','.join(query_split)
+        headers = {'content-type': 'application/x-www-form-urlencoded'}
+        req = 'q='+q+"&scopes="+scopes+'&fields='+fields+"&taxid="+tax_id
+        res = requests.post('http://mygene.info/v3/query', data=req, headers=headers)
+        results = json.loads(res.text)
+    else:
+        # If the query is too long, we will need to break it up into chunks of 1000 query genes (MyGene.info cap)
+        if query_n % 1000 == 0:
+            chunks = round(query_n / 1000)
+        else:
+            chunks = round(np.floor(query_n / 1000) + 1)
+        query_chunks = []
+        for i in range(chunks):
+            start_i, end_i = i*1000, (i+1)*1000
+            query_chunks.append(','.join(query_split[start_i:end_i]))
+        results = []
+        for chunk in query_chunks:
+            headers = {'content-type': 'application/x-www-form-urlencoded'}
+            req = 'q='+chunk+"&scopes="+scopes+'&fields='+fields+"&taxid="+tax_id
+            res = requests.post('http://mygene.info/v3/query', data=req, headers=headers)
+            results = results + json.loads(res.text)         
+    print(len(results), 'Matched query results')
+    print('Batch query complete:', round(time.time()-query_time,2), 'seconds')
+    return results
 
 # Construct matched queries maps
 def construct_query_map_table(query_result, query_genes, display_unmatched_queries=False):
+	if type(query_genes) == str:
+		query_genes = query_genes.split(' ')
 	construction_time = time.time()
 	# Construct DataFrame of matched queries (only keep the results for each query where both symbol and entrez id were mapped)
 	matched_data, matched_genes=[], []
@@ -106,19 +108,20 @@ def construct_query_map_table(query_result, query_genes, display_unmatched_queri
 	# Therefore we remove duplicate mappings to create 1-to-1 mappings for query to genes.
 	duplicate_matched_genes = []
 	for gene in query_genes:
-		if type(match_table.ix[gene])==pd.DataFrame:
+		if type(match_table.loc[gene])==pd.DataFrame:
 			duplicate_matched_genes.append(gene)
 	print()
 	print(len(duplicate_matched_genes), "Queries with mutliple matches found")
 	# Construct mapping table of genes with only one full result
 	single_match_genes = [gene for gene in query_genes if gene not in duplicate_matched_genes]
-	match_table_single = match_table.ix[single_match_genes]
+
+	match_table_single = match_table.loc[single_match_genes]
 	# Keep matches of queries matched only once if there are duplicate matches for genes
 	if len(duplicate_matched_genes) > 0:
 		# Keep maximum scored matches of queries matched more than once
 		max_score_matches=[]
 		for gene in duplicate_matched_genes:
-			matched_duplicates = match_table.ix[gene]
+			matched_duplicates = match_table.loc[gene]
 			max_score = max(matched_duplicates['Score'])
 			max_score_matches.append(matched_duplicates[matched_duplicates['Score']==max_score])
 		match_table_duplicate_max = pd.concat(max_score_matches)
@@ -148,11 +151,21 @@ def filter_query_edgelist(query_edgelist, invalid_genes):
 
 # Convert network edge lists
 # Third column is for weights if desired to pass weights forward
-def convert_edgelist(query_edgelist, gene_map, weighted=False):
-	if weighted:
-		return [sorted([gene_map[edge[0]],gene_map[edge[1]]])+[edge[2]] for edge in query_edgelist]	
-	else:
-		return [sorted([gene_map[edge[0]],gene_map[edge[1]]]) for edge in query_edgelist]
+def convert_edgelist(query_edgelist, query_to_symbol, weighted=False, fill_na=False):
+    edge_df = pd.DataFrame(query_edgelist, columns=["n1", "n2", "weight"])
+    gene_map = pd.DataFrame.from_dict(query_to_symbol, orient='index')
+    gene_map.columns=['symbol']
+    edge_df = edge_df.merge(gene_map, left_on="n1", right_index=True, how="left", suffixes=("", "_n1"))
+    edge_df = edge_df.merge(gene_map, left_on="n2", right_index=True, how="left", suffixes=("", "_n2"))
+    edge_df.columns = ["n1", "n2", "weight", "symbol_n1", "symbol_n2"]
+    if fill_na:
+        edge_df.loc[edge_df.symbol_n1.isna(), "symbol_n1"] = edge_df.loc[edge_df.symbol_n1.isna(), ("n1")]
+        edge_df.loc[edge_df.symbol_n1.isna(), "symbol_n2"] = edge_df.loc[edge_df.symbol_n1.isna(), ("n2")]
+    if weighted:
+        edge_df = edge_df.loc[:, ("symbol_n1", "symbol_n2", "weight")]
+    else:
+        edge_df = edge_df.loc[:, ("symbol_n1", "symbol_n2")]
+    return edge_df
 
 # Sometimes each node needs to be converted by its best match if there are multiple names per node
 # This function uses the match_table constructed earlier to convert genes to either symbol or entrez format only
@@ -183,33 +196,27 @@ def filter_converted_edgelist(edgelist, remove_self_edges=True, weighted=False):
 	print(len(edgelist),'input edges')
 	# Remove self-edges
 	if remove_self_edges:
-		edgelist_filt1 = [edge for edge in edgelist if edge[0]!=edge[1]]
+		edgelist_filt1 = edgelist.loc[(edgelist.symbol_n1 != edgelist.symbol_n2)]
 		print(len(edgelist)-len(edgelist_filt1), 'self-edges removed')
 	else:
 		edgelist_filt1 = edgelist
 		print('Self-edges not removed')
 	if weighted:
 		# Remove edges where one or both nodes are "None"
-		edgelist_filt2 = pd.DataFrame(data=edgelist_filt1).dropna().values.tolist()
+		edgelist_filt2 = edgelist_filt1.dropna()
 		print(len(edgelist_filt1)-len(edgelist_filt2), 'edges with un-mapped genes removed')
 		# Remove duplicates by keeping the max score
 		edgelist_filt3_scoremap = {}
-		for edge in edgelist_filt2:
-			if edge[0]+'+'+edge[1] not in edgelist_filt3_scoremap:
-				edgelist_filt3_scoremap[edge[0]+'+'+edge[1]] = edge[2]
-			else:
-				edgelist_filt3_scoremap[edge[0]+'+'+edge[1]] = max(edgelist_filt3_scoremap[edge[0]+'+'+edge[1]], edge[2])
+		edgelist_filt2 = edgelist_filt2.sort_values(by="weight", ascending=False)
+		edgelist_filt3 = edgelist_filt2.drop_duplicates(subset=['symbol_n1', 'symbol_n2'], keep='first')
 		# Convert dictionary of scores to list
-		edgelist_filt3 = []
-		for edge in edgelist_filt3_scoremap:
-			edgelist_filt3.append(edge.split('+')+[edgelist_filt3_scoremap[edge]])
 		print(len(edgelist_filt2)-len(edgelist_filt3), 'duplicate edges removed')
 	else:
 		# Remove edges where one or both nodes are "None"
-		edgelist_filt2 = pd.DataFrame(data=edgelist_filt1).dropna()
-		print(len(edgelist_filt1)-edgelist_filt2.shape[0], 'edges with un-mapped genes removed')
+		edgelist_filt2 = edgelist_filt1.dropna()
+		print(len(edgelist_filt1)-len(edgelist_filt2), 'edges with un-mapped genes removed')
 		# Remove duplicate edges
-		edgelist_filt3 = edgelist_filt2.drop_duplicates().values.tolist()
+		edgelist_filt3 = edgelist_filt2.drop_duplicates()
 		print(edgelist_filt2.shape[0]-len(edgelist_filt3), 'duplicate edges removed')
 	print('Edge list filtered:',round(time.time()-filter_time,2),'seconds')
 	print(len(edgelist_filt3), 'Edges remaining')
@@ -217,12 +224,18 @@ def filter_converted_edgelist(edgelist, remove_self_edges=True, weighted=False):
 
 # Write edgelist to file
 def write_edgelist(edgelist, output_file, delimiter='\t', binary=True):
-	write_time=time.time()
-	f = open(output_file,'w')
-	for edge in edgelist:
-		if binary:
-			f.write(delimiter.join([edge[0], edge[1]])+'\n')
-		else:
-			f.write(delimiter.join([str(val) for val in edge])+'\n')
-	f.close()
-	print('Edge list saved:', round(time.time()-write_time,2),'seconds')
+    write_time=time.time()
+    if type(edgelist) == list:
+        f = open(output_file,'w')
+        for edge in edgelist:
+            if binary:
+                f.write(delimiter.join([edge[0], edge[1]])+'\n')
+            else:
+                f.write(delimiter.join([str(val) for val in edge])+'\n')
+        f.close()
+    else:
+        if binary:
+            edgelist.loc[:, ("symbol_n1", "symbol_n2")].to_csv(output_file, sep=delimiter, index=False, header=False)
+        else:
+            edgelist.to_csv(output_file, sep=delimiter, index=False, header=False)
+    print('Edge list saved:', round(time.time()-write_time,2),'seconds')
