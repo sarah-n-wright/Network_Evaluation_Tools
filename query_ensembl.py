1b74daa8838331a05d7d81c28a0150b6e79ffbbb
sarah-n-wright <sarahwright474@gmail.com>
2023-01-03 09:52:59 -0800

    Improved performace of ensembl id updating

--- query_ensembl.py
+++ query_ensembl.py
@@ -5,6 +5,42 @@ import json
 def get_latest_ensembl_id(ids):
     server = "https://rest.ensembl.org"
     ext = "/archive/id"
+    ids = list(ids)
+    headers={ "Content-Type" : "application/json", "Accept" : "application/json"}
+    # Initialize an empty list to store the results
+    results_df_list = []
+    # Split the list of IDs into batches of 1000
+    for i in range(0, len(ids), 100):
+        print("Query batch", i, "-", min(i+100, len(ids)))
+        batch_ids = ids[i:min(i+100, len(ids))]
+        # Send the API request with the current batch of IDs with retry logic
+        retries = 5
+        session = requests.Session()
+        session.mount('https://', requests.adapters.HTTPAdapter(max_retries=retries))
+        r = session.post(server+ext, headers=headers, data='{ "id" :' + json.dumps(list(batch_ids)) +'}')
+        #r = requests.post(server+ext, headers=headers, data='{ "id" :' + json.dumps(list(batch_ids)) +'}')
+        if not r.ok:
+            #print(r.request)
+            #print("\n".join(batch_ids))
+            r.raise_for_status()
+            sys.exit()
+        # Process the API response
+        decoded = r.json()
+        decoded_df = pd.DataFrame.from_dict(decoded)
+        decoded_df["to"] = decoded_df.apply(parse_archive_results, axis=1)
+        batch_results_df = decoded_df.loc[:, ("id", "to")]
+        batch_results_df.columns = ["from", "to"]
+        results_df_list.append(batch_results_df)
+    # Concatenate all the results into a single DataFrame
+    results_df = pd.concat(results_df_list)
+    # Find the IDs that were missing from the API response
+    missing = [node for node in ids if node not in results_df['from'].values]
+    return results_df, missing
+
+
+def get_latest_ensembl_id_old(ids):
+    server = "https://rest.ensembl.org"
+    ext = "/archive/id"
     headers={ "Content-Type" : "application/json", "Accept" : "application/json"}
     #r = requests.post(server+ext, headers=headers, data='{ "id" : ["ENSG00000157764", "ENSG00000248378"] }')
     r = requests.post(server+ext, headers=headers, data='{ "id" :' + json.dumps(list(ids)) +'}')
