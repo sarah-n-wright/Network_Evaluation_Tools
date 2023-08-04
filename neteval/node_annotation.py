6cd21c104013fc7e7d8b1109fff0fa5dfb9c04b1
sarah-n-wright <sarahwright474@gmail.com>
2023-08-04 14:04:01 -0700

    Fixed uniprot API for annotation features

--- neteval/node_annotation.py
+++ neteval/node_annotation.py
@@ -23,7 +23,7 @@ def get_node_database_counts(file_list, id_type="Entrez"):
                     node_dict[line.strip()] += 1 # add as string
     return node_dict
 
-def get_uniprot_annotation_data(fields, outdir, page_size=500, max_retries=5, taxid=9606, verbose=False):
+def get_uniprot_annotation_data(fields, outdir, index_on=2, page_size=500, max_retries=5, taxid=9606, verbose=False):
     if taxid != 9606:
         raise(NotImplementedError("Only human data is currently supported"))
     re_next_link = re.compile(r'<(.+)>; rel="next"')
@@ -33,13 +33,26 @@ def get_uniprot_annotation_data(fields, outdir, page_size=500, max_retries=5, ta
     return_fields = ",".join(fields)
     url = 'https://rest.uniprot.org/uniprotkb/search?fields='+return_fields+'&format=tsv&query=%28%28organism_id%3A9606%29%29&size=' + str(page_size)
     results = {}
+    i = 0
     for batch, total in get_batch(url, re_next_link, session, verbose=verbose):
+        i += 1
+        print("batch", str(i))
         for line in batch.text.splitlines()[1:]:
             data = line.split("\t")
-            if len(data[0]) > 0 :
-                geneid = data[0].split(";")
-                if geneid[1] == '':
-                    results[int(geneid[0])] = {'aa_length':int(data[1]), 'mass':int(data[2])}
+            if len(data[index_on]) > 0 :
+                if ";" in data[index_on]:
+                    geneid = data[index_on].split(";")
+                    if geneid[1] != '':
+                        continue
+                    else:
+                        geneid = geneid[0]
+                else:
+                    geneid = data[index_on]
+                try:
+                    results[int(geneid)] = {fields[i]: data[i] for i in range(len(fields))}
+                except:
+                    print(data)
+                    raise ValueError
     results_df = pd.DataFrame.from_dict(results, orient='index')
     
     file_name = 'uniprot_data_'+"_".join(fields) + "_" + datetime.today().strftime('%Y-%m-%d')
@@ -66,3 +79,8 @@ def get_batch(batch_url, re_next_link, session, verbose=False):
         yield response, total
         batch_url = get_next_link(response.headers, re_next_link)
 
+# if __name__ == '__main__':
+#     fields = ['id', 'sequence', 'xref_geneid', 'xref_ensembl']
+#     #fields = ['id', 'length', 'mass']
+#     outdir = '/cellar/users/snwright/Data/Network_Analysis/Reference_Data/'
+#     get_uniprot_annotation_data(fields, outdir, index_on=2)
